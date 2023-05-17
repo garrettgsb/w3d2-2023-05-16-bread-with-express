e26d5c2694a5411868251a83af6a2781fa36b170
Garrett <garrettgsb@gmail.com>
2023-05-17 12:30:26 -0700

    After lecture

--- 00-a-bunch-of-functions.js
+++ 00-a-bunch-of-functions.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 
 function readCounter() { return Number(fs.readFileSync('./counter')); }
-function writeCounter(value) { return fs.writeFileSync('./counter', value);  }
+function writeCounter(value) { return fs.writeFileSync('./counter', String(value));  }
 
 function increment() { writeCounter(readCounter() + 1); }
 function decrement() { writeCounter(readCounter() - 1); }
 function setTo(value) { writeCounter(Number(value)); }
 function reset() { setTo(0); }
 
+/*
+UI - User interface
+API - Application programming interface
+*/
 
 // Mapping between Javascript functions and command line interface
 {
   const [_interpreter, _file, operation, value] = process.argv;
-  console.log('Args:', process.argv);
   if (operation === 'increment') { increment(); console.log(`After ${operation}: ${readCounter()}`)}
   else if (operation === 'decrement') { decrement(); console.log(`After ${operation}: ${readCounter()}`)}
   else if (operation === 'set') { setTo(value); console.log(`After ${operation}: ${readCounter()}`)}
@@ -20,33 +23,35 @@ function reset() { setTo(0); }
   else { console.log(`Count is: ${readCounter()}`) }
 }
 
-//Mapping between Javascript functions and HTTP interface
+// Mapping between Javascript functions and HTTP interface
 {
   const express = require('express');
   const app = express();
   const PORT = 3000;
 
+  app.use(express.urlencoded({ extended: true }));
+
   app.get('/', (request, response) => { response.send(`
     <h1>Count is: ${readCounter()}</h1>
     <a href='/increment'>Increment</a>
     <a href='/decrement'>Decrement</a>
+    <a href='/reset'>Reset</a>
     <form method='GET' action='/reset'>
       <button>Reset</button>
     </form>
-    <form method='GET' action='/set'>
+    <form method='POST' action='/set'>
       <input name='newCounterValue'>
       <button>Set</button>
     </form>
   `)});
-
   app.get('/increment', (request, response) => { increment(); response.redirect('/') });
   app.get('/decrement', (request, response) => { decrement(); response.redirect('/') });
   app.get('/reset', (request, response) => { reset(); response.redirect('/') });
-  app.get('/set', (request, response) => {
-    const { newCounterValue } = request.query;
+  app.post('/set', (request, response) => {
+    const { newCounterValue } = request.body;
     if (newCounterValue) setTo(newCounterValue);
-    response.redirect('/')
+    response.redirect('/');
   });
 
-  // app.listen(8080, () => { console.log(`Listening on ${PORT}!`)});
+  app.listen(PORT, () => { console.log(`Listening on ${PORT}!`)});
 }
--- 01-todo-list.js
+++ 01-todo-list.js
@@ -1,43 +1,64 @@
-let todos = [
+const todos = [
   "Get milk",
   "Wash car",
   "Walk dog",
 ];
 
-function addTodo(todo) {
-  todos.push(todo);
+function addTodo(newText) {
+  todos.push(newText);
 }
 
-function removeTodo(idx) {
+function deleteTodo(idx) {
   if (!todos[idx]) throw new Error(`No todo at index ${idx}!`);
-  todos.splice(idx, 1);
+  todos.splice(Number(idx), 1);
 }
 
-function updateTodo(idx, newText) {
+function editTodo(idx, newText) {
   if (!todos[idx]) throw new Error(`No todo at index ${idx}!`);
   todos[idx] = newText;
 }
 
-function viewTodos() {
-  return `<h1>Todos:</h1>
+function browseTodos() {
+  return `
+  <h1>Todos:</h1>
   <ul>
   ${todos.map((todo, idx) => `
     <li>
-    ${todo} [${idx}]
+      ${todo} [${idx}]
       <form method='POST' action='/todos/${idx}'>
-        <input name='newText'>
-        <button>Edit</button>
+        <input name='newTodoText'>
+        <button>🖋</button>
       </form>
       <form method='POST' action='/todos/${idx}/delete'>
-        <button>🚮</button>
+      <button>🚮</button>
       </form>
     </li>
   `).join('\n')}
   </ul>
-
   <form method='POST' action='/todos'>
-    <input name='todo'>
-    <button>+ Add</button>
+    <input name='newTodoText'>
+    <button>➕</button>
   </form>
   `;
 }
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => { response.send(browseTodos()) });
+app.post('/todos/:id', (request, response) => {
+  const id = request.params.id;
+  const newTodoText = request.body.newTodoText;
+  editTodo(id, newTodoText);
+  response.redirect('/');
+});
+
+app.post('/todos/:id/delete', (request, response) => { deleteTodo(request.params.id); response.redirect('/') });
+app.post('/todos', (request, response) => {
+  const newTodoText = request.body.newTodoText;
+  addTodo(newTodoText);
+  response.redirect('/');
+});
+app.listen(8080);
